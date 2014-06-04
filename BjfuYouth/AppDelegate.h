c5a168660c881e85ebc19f2e8a2da08801e82244
yu <yu@yus-MacBook-Pro.local>
2014-06-04 20:27:13 +0800

    Initial Commit

---
+++ BjfuYouth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BjfuYouth
+//
+//  Created by yu on 6/4/14.
+//  Copyright (c) 2014 yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
